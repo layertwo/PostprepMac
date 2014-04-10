a81b1eed5f9c0a7ef10f3f11fc3e997c13a993b1
Lucas Messenger <lucas.c.messenger@gmail.com>
2014-04-09 22:35:58 -0400

    Variable updates
    Declares mounted directories variably, does not matter if name updates.

--- PostprepMac.sh
+++ PostprepMac.sh
@@ -4,7 +4,7 @@
 # author: Lucas Messenger
 # version: 0.1.2
 # created: 02_24_2014
-# modified: 02_26_2014
+# modified: 04_09_2014
 #
 #
 # Notes:
@@ -14,13 +14,12 @@
 # But requires Adobe distribution license (looks like it's free)
 #
 
-dmgNames=( "Flash.dmg" "Reader.dmg" "AIR.dmg" "Shockwave.dmg" "Silverlight.dmg" "Firefox.dmg" "Java.dmg" )
-volNames=( "Adobe Flash Player Installer" "Adobe Reader Installer" "Adobe AIR" "Adobe Shockwave 12" "Silverlight" "Firefox" "Java 7 Update 51" )
+DMGNAMES=( "Flash.dmg" "Reader.dmg" "AIR.dmg" "Shockwave.dmg" "Silverlight.dmg" "Firefox.dmg" "Java.dmg" )
 
 clear
 uid=$(id -u) #check to see if running in root
 if [ "$uid" != "0" ]; then
-	echo -e "Postprep for Mac must be run as root user. Please try again."
+	echo "Postprep for Mac must be run as root user. Please try again."
 	exit
 fi
 
@@ -29,20 +28,20 @@ cd ~/postprep_temp
 
 # Get Flash, Reader, AIR, and Shockwave, Silverlight, and Firefox with curl and progress bar
 echo "Downloading installers...\n"
-echo "Downloading Adobe Flash Player..."
-curl --progress-bar -o Flash.dmg http://aihdownload.adobe.com/bin/live/AdobeFlashPlayerInstaller_12_ltrosxd_aaa_aih.dmg
-echo "Downloading Adobe Reader..."
+echo "(1/7) Downloading Adobe Flash Player..."
+curl --progress-bar -o Flash.dmg http://aihdownload.adobe.com/bin/live/AdobeFlashPlayerInstaller_13_ltrosxd_aaa_aih.dmg
+echo "(2/7) Downloading Adobe Reader..."
 curl --progress-bar -o Reader.dmg http://aihdownload.adobe.com/bin/live/AdobeReaderInstaller_11_en_ltrosxd_aaa_aih.dmg
-echo "Downloading Adobe AIR..."
+echo "(3/7) Downloading Adobe AIR..."
 curl --progress-bar -o AIR.dmg http://airdownload.adobe.com/air/mac/download/latest/AdobeAIR.dmg
-echo "Downloading Adobe Shockwave..."
+echo "(4/7) Downloading Adobe Shockwave..."
 curl --progress-bar -o Shockwave.dmg http://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Full_64bit.dmg
-echo "Downloading Microsoft Silverlight..."
+echo "(5/7) Downloading Microsoft Silverlight..."
 curl --progress-bar -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36" \
     -Lo Silverlight.dmg http://www.microsoft.com/getsilverlight/handlers/getsilverlight.ashx
-echo "Downloading Mozilla Firefox..."
+echo "(6/7) Downloading Mozilla Firefox..."
 curl --progress-bar -L -o Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
-echo "Downloading Java..."
+echo "(7/7) Downloading Java..."
 curl --progress-bar -L -o Java.dmg "http://javadl.sun.com/webapps/download/AutoDL?BundleId=83377"
 sleep 1
 echo "Installers downloaded.\n"
@@ -50,16 +49,31 @@ sleep 1
 
 # Mount downloaded installers
 echo "Mounting installers..."
-for dmg in ${dmgNames[@]} 
+for DMG in "${DMGNAMES[@]}"
 do
-	hdiutil attach $dmg -nobrowse -quiet
-	echo "$dmg is mounted."
+	hdiutil attach "$DMG" -nobrowse -quiet
+	echo "$DMG is mounted."
 done
 sleep 1
 echo "Installers are mounted.\n"
 sleep 1
-
 clear
+
+# Find dynamic directories, and only the top directory
+# I only need to know where they reside in /Volumes
+FLSH=$(find /Volumes -type d -name "*Flash*" -maxdepth 1)
+RDR=$(find /Volumes -type d -name "*Reader*" -maxdepth 1)
+AIR=$(find /Volumes -type d -name "*AIR*" -maxdepth 1)
+FRFX=$(find /Volumes -type d -name "*Firefox*" -maxdepth 1)
+SHKWV=$(find /Volumes -type d -name "*Shockwave*" -maxdepth 1)
+SLVR=$(find /Volumes -type d -name "*Silverlight*" -maxdepth 1)
+JAVA=$(find /Volumes -type d -name "*Java*" -maxdepth 1)
+
+# Get .pkg location for folder that have it
+SHKWVPKG=$(find "$SHKWV" -name '*Shockwave*.pkg')
+SLVRPKG=$(find "$SLVR" -name '*Silverlight*.pkg')
+JAVAPKG=$(find "$JAVA" -name '*Java*.pkg')
+
 # Install from mounted volumes
 echo "postprep: installing Firefox..."
 cp -R /Volumes/Firefox/Firefox.app /Applications
@@ -67,17 +81,19 @@ echo "postprep: Firefox installed."
 /Volumes/Adobe\ Flash\ Player\ Installer/Install\ Adobe\ Flash\ Player.app/Contents/MacOS/Install\ Adobe\ Flash\ Player
 /Volumes/Adobe\ Reader\ Installer/Install\ Adobe\ Reader.app/Contents/MacOS/Install\ Adobe\ Reader
 /Volumes/Adobe\ AIR/Adobe\ AIR\ Installer.app/Contents/MacOS/Adobe\ AIR\ Installer
-installer -pkg /Volumes/Adobe\ Shockwave\ 12/Shockwave_Installer_Full.pkg -target /
-installer -pkg /Volumes/Silverlight/Silverlight.pkg -target /
-installer -pkg /Volumes/Java\ 7\ Update\ 51/Java\ 7\ Update\ 51.pkg -target /
+installer -pkg "$SHKWVPKG" -target /
+installer -pkg "$SLVRPKG" -target /
+installer -pkg "$JAVAPKG" -target /
 sleep 1
 
-#Unmount installers
+VOLNAMES=( "$FLSH" "$RDR" "$AIR" "$FRFX" "$SHKWV" "$SLVR" "$JAVA" )
+
+# Unmount installers
 echo "Unmounting installers..."
-for vol in ${volNames[@]}
+for VOL in "${VOLNAMES[@]}"
 do
-    hdiutil detach /Volumes/$name -quiet
-    echo "$vol is unmounted."
+    hdiutil detach "$VOL" -quiet
+    echo "$VOL is unmounted."
 done
 sleep 1
 echo "Installers are unmounted.\n"
